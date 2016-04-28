3330bc3f2b0f2886cdf68f2d9b26e98796ac5db7
Ian Leow <blackhydrogen@users.noreply.github.com>
2016-04-28 13:18:50 +0800

    Update runproj.bat

--- misc/runproj.bat
+++ misc/runproj.bat
@@ -2,5 +2,3 @@
 start "Main Program" /D "C:\Users\Ian\WorkspaceGeneral\resaVLDTopology\target" echo storm jar resa-vld-1.0-SNAPSHOT-jar-with-dependencies.jar topology.VLDTopFox "C:/Users/Ian/Desktop/FYP2/vld_conf.yaml"
 start "Input" /D "C:\Users\Ian\WorkspaceGeneral\resaVLDTopology\target" echo storm jar resa-vld-1.0-SNAPSHOT-jar-with-dependencies.jar tool.SimpleImageSenderFox C:\Users\Ian\Desktop\FYP2\vld_conf.yaml fSource 0 10 1
 start "Output" /D "C:\Users\Ian\WorkspaceGeneral\resaVLDTopology\target" echo storm jar resa-vld-1.0-SNAPSHOT-jar-with-dependencies.jar server.TomVideoStreamToFile localhost 6379 tomQ
-
-rem ffmpeg -framerate 4 -start_number 1 -i frame%06d.jpg -vframes 100000 -r 25 -f mpegts - | ffplay -
